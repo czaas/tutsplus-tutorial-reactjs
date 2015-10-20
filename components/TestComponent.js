dd190c444b2bb4a7502c61b93588b88cb088424a
Cameron Zaas <cameronzaas@gmail.com>
2015-10-20 06:48:03 -0700

    Have a working input box to add new items to an array. Sort of like a todo list

--- components/TestComponent.js
+++ components/TestComponent.js
@@ -10,10 +10,25 @@ var TestComponent = React.createClass({
 });
 
 var MessageBox = React.createClass({
+	handleAdd: function(e){
+		var newMessage = this.refs.newMessage.getDOMNode().value;
+		var newMessages = this.state.messages.concat([newMessage]);
+
+		this.setState({
+			messages: newMessages
+		});
+	},
+
 	getInitialState: function(){
 		return {
 			isVisible: true,
-			titleMessage: 'Hello world!'
+			titleMessage: 'Hello world!',
+			messages: [
+				'Hello you!',
+				'This weather has be crazy, right?',
+				'Fine, dont answer me.',
+				'I dont want to talk to you anyways.'
+			]
 		};
 	},
 
@@ -23,20 +38,36 @@ var MessageBox = React.createClass({
 			display: this.state.isVisible ? 'block' : 'none'
 		};
 
+		var messages = this.state.messages.map(function(message){
+			return <SubMessage message={message} />;
+		});
+
 		return (
 			<div className="container jumbotron" style={inlineStyles}>
 				<h2>{this.state.titleMessage}</h2>
-
-				<SubMessage />
+				<input type="text" ref="newMessage" />
+				<button className="btn btn-primary" onClick={this.handleAdd}>Add</button>
+				{ messages }
 			</div>
 		);
 	}
 });
 
 var SubMessage = React.createClass({
+
+	propTypes: {
+		message: React.PropTypes.string.isRequired // validation of sorts: This must be a string and it IS required.
+	},
+
+	getDefaultProps: function(){
+		return {
+			message: 'It is good to see you.'
+		};
+	},
+
 	render: function(){
 		return (
-			<div>It is nice to see you!</div>
+			<div>{this.props.message}</div>
 		);
 	}
 });
